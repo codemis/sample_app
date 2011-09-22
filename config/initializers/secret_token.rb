8de7960ed1beebcccdae2357e7d221d0974c8cf6
Codemis <johnathan@still-water.com>
2011-09-22 10:25:02 -0700

    Initial code library.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SampleApp::Application.config.secret_token = '5256ff8ef68ea833d672f60399dbd1b17ddb4fa540066d166f90136da83ba5c9bc2e24304b76510d253760c7ffd78ce1f1a81673c21c52f42284de881deb85bb'
