ba36461f4967684d6b2deb90cf1422ce6f4f721e
Codemis <johnathan@still-water.com>
2011-09-22 14:08:40 -0700

    Moved title into a helper.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,14 @@
 module ApplicationHelper
+  
+  # get the page title
+  #
+  def title
+    base_title = "Ruby on Rails Tutorial Sample App"
+    if @title.nil?
+      base_title
+    else
+      "#{base_title} | #{@title}"
+    end
+  end
+  
 end
